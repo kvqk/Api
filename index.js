090d7d1a644b8ecb3db473897a892785ad6a960e
Tyler <tyler@class-computers-MacBook-Air.local>
2022-06-05 14:31:44 -0700

    h

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const mainCard = document.getElementById("card");
+
 async function getData() {
   const response = await fetch(
     "https://anime-facts-rest-api.herokuapp.com/api/v1"
@@ -6,6 +8,21 @@ async function getData() {
   const res = await response.json();
   res?.data.map((anime) => {
     console.log(anime);
+    const div = document.createElement("div");
+    div.id = "indiviual card";
+    div.style.width = "500px";
+    div.style.height = "500px";
+
+    const image = document.createElement("img");
+    image.src = anime.anime_img;
+
+    const nameSpan = document.createElement("span");
+    nameSpan.textContent = anime.anime_name;
+
+    div.append(image);
+    div.append(nameSpan);
+
+    mainCard.append(div);
   });
 }
 
